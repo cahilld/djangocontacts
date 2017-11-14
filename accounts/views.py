ec424ea3b5bfafd004f4d44ed90aa666cfa1978b
DesC <cahill.desmond@gmail.com>
2017-11-14 14:57:42 +0000

    reg form upload

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 def get_index(request):
@@ -17,14 +17,29 @@ def login(request):
         if form.is_valid():
             user = auth.authenticate(username=form.cleaned_data['username'],
                                      password=form.cleaned_data['password'])
-
             if user is not None:
                 auth.login(request, user)
             else:
                 form.add_error(None, "Your details are not valid")
             return redirect(get_index)
-        
     else:
         form = UserLoginForm()
     
     return render(request, "login.html", {'form': form})
+    
+def register(request):
+    if request.method=="POST":
+        form = UserRegistrationForm(request.POST)
+    
+        if form.is_valid():
+            user = form.save()
+            user = auth.authenticate(username=form.cleaned_data['username'],
+                             password=form.cleaned_data['password1'])
+            if user is not None:
+                auth.login(request, user)
+                return redirect(get_index)
+                
+    else:
+        form = UserRegistrationForm()
+        
+    return render(request, "register.html", {'form': form})
